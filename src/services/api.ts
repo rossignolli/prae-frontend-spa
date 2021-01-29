161ab09459cb7ccf2774a4dcf95e4787041dbf14
Vitor Vigarani Rossignolli <34142721+rossignolli@users.noreply.github.com>
2021-01-28 22:30:31 -0300

    url fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://api.vigarani.codes/'
 });
 
 export default api;
+
+//baseURL: 'http://localhost:3333'
