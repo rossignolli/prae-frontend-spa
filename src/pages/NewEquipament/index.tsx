2a95d7ee71511596e96b5959be6626952ebc1540
Vitor <vitor@canaltech.com.br>
2021-07-13 22:21:56 -0300

    feature: inicia nova pagina de equipamentos

--- src/pages/NewEquipament/index.tsx
+++ src/pages/NewEquipament/index.tsx
@@ -1,114 +1,78 @@
-import React, {useEffect, useState} from 'react'
-import { FiAlertTriangle, FiAlignLeft, FiBarChart, FiBell, FiDelete, FiEdit2, FiHardDrive, FiPlus } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import {
+  FiAlertTriangle,
+  FiAlignLeft,
+  FiBarChart,
+  FiBell,
+  FiDelete,
+  FiEdit2,
+  FiHardDrive,
+  FiPlus,
+} from "react-icons/fi";
+import { Link } from "react-router-dom";
 
-import manProfile from '../../assets/temp_assets/man-profile.jpg'
-import { ButtonPurple, ButtonPurpleInverted } from '../../components/button/styles';
-import NavigationBar from '../../components/navbar';
+import manProfile from "../../assets/temp_assets/man-profile.jpg";
+import {
+  ButtonPurple,
+  ButtonPurpleInverted,
+} from "../../components/button/styles";
+import NavigationBar from "../../components/navbar";
 
 // import { useAuth } from '../../hooks/AuthContext';
-import api from '../../services/api';
-import { HeadersContents } from '../Dashboard/styles';
-import { NewEquipamentsContent, Container } from './styles';
+import api from "../../services/api";
+import { HeadersContents } from "../Dashboard/styles";
+import {
+  NewEquipamentsContent,
+  Container,
+  ProgressBarContainer,
+  NewEquipamentsContent2,
+} from "./styles";
 
+const NewEquipament: React.FC = () => {
+  return (
+    <Container>
+      <NavigationBar />
+      <NewEquipamentsContent2>
+        <ProgressBarContainer>
+          <div>
+            <h1>1</h1>
+            <span>Informações Basicas</span>
+          </div>
+          <div>
+            <h1>2</h1>
+            <span>Informações Basicas</span>
+          </div>
+          <div>
+            <h1>3</h1>
+            <span>Informações Basicas</span>
+          </div>
+          <div>
+            <h1>4</h1>
+            <span>Informações Basicas</span>
+          </div>
+        </ProgressBarContainer>
+      </NewEquipamentsContent2>
+      <NewEquipamentsContent>
+        <h1>Informações Básica do Equipamento</h1>
+        <p>
+          Insira os dados do equipamento que será adicionado na base de dados de
+          monitoramento. Todo equipamento por padrão é adicionado com
+          monitoramento desligado
+        </p>
+        <form action="">
+          <div className="input-block">
+            <label htmlFor="about">Nome do equipamento</label>
+            <input type="text" />
+          </div>
+        </form>
 
-  
-
-
-  
-
-
-const NewEquipament: React.FC = ()=>{
-
-
-    return(
-        <Container>
-          <NavigationBar/>
-          <NewEquipamentsContent>
-
-            <p>Insira os dados do equipamento que será adicionado na base de dados de monitoramento. 
-              Todo equipamento por padrão é adicionado com monitoramento desligado</p>  
-              <form action="">
-              <div className="input-block">
-              <label htmlFor="about">Nome do equipamento</label>
-              <input type="text"/>
-              </div>   
-
-              <div className="input-block">
-              <label htmlFor="about">Descrição</label>
-              <textarea/>
-              </div>   
-
-
-               <p className="warning">*Os tipos de equipamentos são importantes para atribuir os procedimentos corretos criado pelo administrador para cada tipo de equipamento
-                 durante um eventual procedimento de preventiva ou corretiva.</p>   
-
-
-              <div className="input-select-block">
-              <label htmlFor="brands">Escolha o tipo do equipamento</label>
-                <select name="brands" > 
-                <option value="apple">Refrigeração</option>
-                <option value="dell">Porteiro Eletronico</option>
-                <option value="hp">Câmera de Segurança</option>
-                <option value="hp">Switch de rede</option>
-                <option value="lenovo">Estação de Trabalho</option>
-                <option value="lenovo">Notebook</option>
-                </select>
-              </div>   
-
-              <div className="input-select-block">
-              <label htmlFor="brands">Escolha a marca</label>
-                <select name="brands" > 
-                <option value="apple">Apple</option>
-                <option value="dell">Dell Inc</option>
-                <option value="hp">HP</option>
-                <option value="lenovo">Lenovo</option>
-                </select>
-              </div>   
-
-              <label htmlFor="about">Prioridade do equipamento</label>
-
-              <p className="warning-equip">Equipamentos classificados como críticos recebem prioridades em exibição de lista,
-              classificação prioriatária em planejamentos de preventivas e alertas de vencimento em tempo real</p>  
-
-              <div className="input-block-check">
-              <input type="checkbox" id="checkbox-style"/>
-              <label htmlFor="checkbox-style">Crítico</label>
-              </div>  
-              
-
-              <div className="input-select-block">
-              <input multiple type="file" id="image[]"/>
-              </div>   
-
-
-              <div className="input-select-block">
-              <label htmlFor="brands">Nível para gerenciar</label>
-                <select name="brands"> 
-                <option value="apple">Supervisor</option>
-                <option value="dell">Gerente</option>
-                <option value="hp">Técnico</option>
-                </select>
-              </div>   
-
-             
-
-
-              </form>
-            
-              <div className="button-holder">
-              <ButtonPurpleInverted>Voltar</ButtonPurpleInverted>
-              <ButtonPurple>Adicionar</ButtonPurple>
-              </div>
-              
-              
-          
-          </NewEquipamentsContent>
-           
-
-
-        </Container>
-    )
-}
+        <div className="button-holder">
+          <ButtonPurpleInverted>Voltar</ButtonPurpleInverted>
+          <ButtonPurple>Adicionar</ButtonPurple>
+        </div>
+      </NewEquipamentsContent>
+    </Container>
+  );
+};
 
 export default NewEquipament;
--- src/pages/NewEquipament/styles.ts
+++ src/pages/NewEquipament/styles.ts
@@ -1,142 +1,166 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const Container = styled.div`
-        display: flex;
-        height: 100%;
-        background-color: #E5E5E5;
-        flex-direction: column;
-
-    `;
-
-
-    
-export const NewEquipamentsContent = styled.div`
-    margin-top: 20px;
-    margin-left: 340px;
-    background-color: #E5E5E5;
-    display: flex;
-    padding: 50px;
-    flex-direction: column;
-    height: 100%;
-    margin-right: 20px;
-    margin-bottom: 20px;
-
-
-    background-color: #FFFFFF;
-    border-radius: 15px;
-
-    div {
-        margin-bottom: 20px;
-    }
-
-
-    // FAZER COMPONENTES DESSES INPUTS PARA USAR EM OUTROS LUGARES
-
-    input {
-        width: 100%;
-        background: #F1F1F1;
-        border-radius: 15px;
-        outline: none;
-        color: #95979A;
-        height: 58px;
-        padding: 10px;
-        border: 1px solid #DDDDDD;
-    }
-    textarea {
-        width: 100%;
-        background: #F1F1F1;
-        border-radius: 15px;
-        outline: none;
-        color: #95979A;
-        height: 58px;
-        padding: 10px;
-        border: 1px solid #DDDDDD;
-        height: 120px;
-    }
-
-    label {
-        display: flex;
-        color: #000000;
-        margin-bottom: 10px;
-        font-size: 16px;
-        font-weight: bold;
-
-    }
-
-    p {
-        color: #95979A;
-        margin-bottom: 20px;
-    }
-
-    select {
-        background: #F1F1F1;
-        border-radius: 15px;
-        outline: none;
-        width: 100%;
-        color: #95979A;
-        height: 58px;
-        padding: 10px;
-        border: 1px solid #DDDDDD;
-        
-        option {
-        color: black;
-         background: #F1F1F1;
-        }
-
-
-
-    }
-
-    .warning {
-        color: #EA5455;
-  }
-
-  .warning-equip{
-    color: #95979A;
-    margin-bottom: 0px;
-
-  }
-
-  
-  .button-holder{
-      display: flex;
-      justify-content: flex-end;
-      margin-top: 40px;
-
-    button{
-        margin-left: 20px;
-    }
-
-  }
-
-  .input-block-check{
-
-      display: flex;
-      align-items: center;
-
-    label {
-        display: inline-block;
-        color: #95979A;
-        font-weight: normal;
-    }
-
-      input {
-          width: auto;
-          margin-bottom: 10px;
-          margin-right: 10px;
-      }
-  }
-
-
-    @media screen and (max-width: 800px) {
-    margin-left: 20px;
-    }   
-
-
-
-
-
-
+  display: flex;
+  height: 100vh;
+  background-color: #f5f5fa;
+  flex-direction: column;
 `;
 
+export const NewEquipamentsContent2 = styled.div`
+  margin-top: 20px;
+  margin-left: 340px;
+  display: flex;
+  flex-direction: column;
+  background-color: #f5f5fa;
+  border-radius: 15px;
+`;
+
+export const NewEquipamentsContent = styled.div`
+  margin-top: 20px;
+  margin-left: 340px;
+  background-color: #e5e5e5;
+  display: flex;
+  padding: 50px;
+  flex-direction: column;
+  margin-right: 20px;
+  margin-bottom: 20px;
+  background-color: #ffffff;
+  border-radius: 15px;
+
+  div {
+    margin-bottom: 20px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 18px;
+    font-weight: 700;
+    margin-bottom: 20px;
+    color: #8257e5;
+  }
+
+  // FAZER COMPONENTES DESSES INPUTS PARA USAR EM OUTROS LUGARES
+
+  input {
+    width: 100%;
+    background: #f1f1f1;
+    border-radius: 15px;
+    outline: none;
+    color: #95979a;
+    height: 58px;
+    padding: 10px;
+    border: 1px solid #dddddd;
+  }
+  textarea {
+    width: 100%;
+    background: #f1f1f1;
+    border-radius: 15px;
+    outline: none;
+    color: #95979a;
+    height: 58px;
+    padding: 10px;
+    border: 1px solid #dddddd;
+    height: 120px;
+  }
+
+  label {
+    display: flex;
+    color: #000000;
+    margin-bottom: 10px;
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  p {
+    color: #95979a;
+    margin-bottom: 20px;
+  }
+
+  select {
+    background: #f1f1f1;
+    border-radius: 15px;
+    outline: none;
+    width: 100%;
+    color: #95979a;
+    height: 58px;
+    padding: 10px;
+    border: 1px solid #dddddd;
+
+    option {
+      color: black;
+      background: #f1f1f1;
+    }
+  }
+
+  .warning {
+    color: #ea5455;
+  }
+
+  .warning-equip {
+    color: #95979a;
+    margin-bottom: 0px;
+  }
+
+  .button-holder {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 40px;
+
+    button {
+      margin-left: 20px;
+    }
+  }
+
+  .input-block-check {
+    display: flex;
+    align-items: center;
+
+    label {
+      display: inline-block;
+      color: #95979a;
+      font-weight: normal;
+    }
+
+    input {
+      width: auto;
+      margin-bottom: 10px;
+      margin-right: 10px;
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    margin-left: 20px;
+  }
+`;
+
+export const ProgressBarContainer = styled.div`
+  display: flex;
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+
+    h1 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 50%;
+      width: 50px;
+      height: 50px;
+      background-color: #8257e5;
+      color: white;
+      margin-bottom: 10px;
+      font-size: 24px;
+      font-weight: 700;
+    }
+
+    span {
+      color: #8257e5;
+      font-size: 16px;
+      font-weight: 700;
+    }
+  }
+`;
