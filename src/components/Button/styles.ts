6430de59f2479467f06852d2b8d0d10a09959e38
Vitor Rossignolli <vitorrossignolli@gmail.com>
2021-08-07 11:19:32 -0300

    refac(button) refactor button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,76 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+import { darken } from 'polished'
+
+import { ButtonProps } from '.'
+
+export type WrapperProps = {
+  hasIcon: boolean
+} & Pick<ButtonProps, 'size' | 'fullWidth' | 'minimal'>
+
+const wrapperModifiers = {
+  small: (theme: DefaultTheme) => css`
+    height: 3rem;
+    font-size: ${theme.font.sizes.xsmall};
+  `,
+  medium: (theme: DefaultTheme) => css`
+    height: 4rem;
+    font-size: ${theme.font.sizes.small};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
+  `,
+  large: (theme: DefaultTheme) => css`
+    height: 5rem;
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+  `,
+  fullWidth: () => css`
+    width: 100%;
+  `,
+  withIcon: (theme: DefaultTheme) => css`
+    svg {
+      width: 1.5rem;
+      & + span {
+        margin-left: ${theme.spacings.xxsmall};
+      }
+    }
+  `,
+  minimal: (theme: DefaultTheme) => css`
+    background: none;
+    color: ${theme.colors.primary};
+    &:hover {
+      color: ${darken(0.1, theme.colors.primary)};
+    }
+  `,
+  disabled: () => css`
+    &:disabled {
+      cursor: not-allowed;
+      filter: saturate(30%);
+    }
+  `
+}
+
+export const Wrapper = styled.button<WrapperProps>`
+  ${({ theme, size, fullWidth, hasIcon, minimal, disabled }) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    background: ${theme.colors.primary};
+    color: ${theme.colors.white};
+    font-family: ${theme.font.family};
+    border: 0;
+    cursor: pointer;
+    border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xxsmall};
+    text-decoration: none;
+    &:focus {
+      box-shadow: 0 0 0 3px ${theme.colors.grayDark};
+    }
+    &:hover {
+      background: ${darken(0.1, theme.colors.primary)}
+    }
+    ${!!size && wrapperModifiers[size](theme)};
+    ${!!fullWidth && wrapperModifiers.fullWidth()};
+    ${!!hasIcon && wrapperModifiers.withIcon(theme)};
+    ${!!minimal && wrapperModifiers.minimal(theme)};
+    ${disabled && wrapperModifiers.disabled()};
+  `}
+`
